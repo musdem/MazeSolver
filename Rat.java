b9348d8c69a777c995d3490eefbe8ecd723c2ab4
Zack Munich <zack.munich@gmail.com>
2014-12-16 17:48:21 -0500

    cleaned up more if statements

--- Rat.java
+++ Rat.java
@@ -10,79 +10,31 @@ public class Rat
 	}
 	public void turnRight()
 	{
-		if(direction == 'u')
-		{
-			direction = 'r';
-		}
-		else if(direction == 'r')
-		{
-			direction = 'd';
-		}
-		else if(direction == 'd')
-		{
-			direction = 'l';
-		}
-		else if(direction == 'l')
-		{
-			direction = 'u';
-		}
+		if(direction == 'u') direction = 'r';
+		else if(direction == 'r') direction = 'd';
+		else if(direction == 'd') direction = 'l';
+		else direction = 'u';
 	}
 	public void turnLeft()
 	{
-		if(direction == 'u')
-		{
-			direction = 'l';
-		}
-		else if(direction == 'r')
-		{
-			direction = 'u';
-		}
-		else if(direction == 'd')
-		{
-			direction = 'r';
-		}
-		else if(direction == 'l')
-		{
-			direction = 'd';
-		}
+		if(direction == 'u') direction = 'l';
+		else if(direction == 'r') direction = 'u';
+		else if(direction == 'd') direction = 'r';
+		else direction = 'd';
 	}
 	public void turnAround()
 	{
-		if(direction == 'u')
-		{
-			direction = 'd';
-		}
-		else if(direction == 'r')
-		{
-			direction = 'l';
-		}
-		else if(direction == 'd')
-		{
-			direction = 'u';
-		}
-		else if(direction == 'l')
-		{
-			direction = 'r';
-		}
+		if(direction == 'u') direction = 'd';
+		else if(direction == 'r') direction = 'l';
+		else if(direction == 'd') direction = 'u';
+		else direction = 'r';
 	}
 	public void travel()
 	{
-		if(direction == 'u')
-		{
-			pos.move(0,-1);
-		}
-		else if(direction == 'r')
-		{
-			pos.move(1,0);
-		}
-		else if(direction == 'd')
-		{
-			pos.move(0,1);
-		}
-		else if(direction == 'l')
-		{
-			pos.move(-1,0);
-		}
+		if(direction == 'u') pos.move(0,-1);
+		else if(direction == 'r') pos.move(1,0);
+		else if(direction == 'd') pos.move(0,1);
+		else pos.move(-1,0);
 	}
 	public Point nextPos()
 	{
